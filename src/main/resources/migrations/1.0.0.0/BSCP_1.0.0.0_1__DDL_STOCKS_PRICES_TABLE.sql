0865ef8badecf576ddc37394f37c6dbdacd40a51
Bruno Paz <soujava@gmail.com>
2021-09-23 11:33:16 -0300

    Changing null column to not-null column

--- src/main/resources/migrations/1.0.0.0/BSCP_1.0.0.0_1__DDL_STOCKS_PRICES_TABLE.sql
+++ src/main/resources/migrations/1.0.0.0/BSCP_1.0.0.0_1__DDL_STOCKS_PRICES_TABLE.sql
@@ -4,9 +4,9 @@ BEGIN
 	
 	CREATE TABLE IF NOT EXISTS stocks.tb_stocks_prices (
     		ticker character varying(8) UNIQUE NOT NULL,
-    		price numeric NULL,
+    		price numeric NOT NULL,
 		currency character(5) NOT NULL,
-		dt_updated timestamp without time zone,
+		dt_updated timestamp without time zone NULL,
     	CONSTRAINT tb_stocks_prices_pk PRIMARY KEY (ticker)
 	) TABLESPACE pg_default;
 																							
